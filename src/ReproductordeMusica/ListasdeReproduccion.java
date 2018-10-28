1fc3eca6ea6959b0d5cbe919d57c910463305ab9
laullorente17 <laura.llorente@opendeusto.es>
2018-10-28 16:38:52 +0100

    Algunos metodos

--- src/ReproductordeMusica/ListasdeReproduccion.java
+++ src/ReproductordeMusica/ListasdeReproduccion.java
@@ -1,5 +1,63 @@
 package ReproductordeMusica;
 
-public class ListasdeReproducción {
+import java.io.File;
+import java.util.ArrayList;
+
+import javax.swing.ListModel;
+import javax.swing.event.ListDataListener;
+
+public class ListasdeReproduccion implements ListModel<String>
+{
+	ArrayList<File> ficheros;
+	
+	/**
+	 * Constructor para iniciar una lista vacia
+	 */
+	public ListasdeReproduccion()
+	{
+		super();
+		ficheros = new ArrayList<File>();
+	}
+	
+	public void añadir (File f)
+	{
+		ficheros.add(f);
+
+	}
+	
+	public void eliminarLista()
+	{
+		ficheros.clear();
+	}
+	
+	public void eliminarFichero (int posi)
+	{
+		ficheros.remove(posi);
+	}
+	
+
+	@Override
+	public void addListDataListener(ListDataListener l) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getElementAt(int index) 
+	{
+		return ficheros.get(index).getName();
+	}
+
+	@Override
+	public int getSize() 
+	{
+		return ficheros.size();
+	}
+
+	@Override
+	public void removeListDataListener(ListDataListener l) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
