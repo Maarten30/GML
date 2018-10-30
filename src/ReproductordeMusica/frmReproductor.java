87cefa5b1d07f9f6cee82c2296b4453dfea37a11
maarten30 <maartenhandels@opendeusto.es>
2018-10-30 13:42:50 +0100

    Interfaz

--- src/ReproductordeMusica/frmReproductor.java
+++ src/ReproductordeMusica/frmReproductor.java
@@ -1,5 +1,54 @@
 package ReproductordeMusica;
 
-public class frmReproductor {
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+
+public class frmReproductor extends JFrame implements ActionListener
+{
+	 
+	private JButton play;
+	private JButton pausa;
+	
+	
+	
+	public static void main(String[] args)
+	{
+		new frmReproductor().GUI();
+	}
+	
+	public void GUI()
+	{
+		JFrame ventanita = new JFrame("Music Player");
+		ventanita.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//ventanita.setSize(300, 200);
+		ventanita.setLayout(new GridLayout(3,1,5,5));
+		
+		pausa = new JButton("Pausa");
+		play = new JButton("Play");
+		play.addActionListener(this);
+		pausa.addActionListener(this);
+		
+		ventanita.add(play);
+		ventanita.add(pausa);
+		ventanita.setVisible(true);
+	
+	}
+	
+	public void actionPerformed(ActionEvent arg0) {
+		if(arg0.getSource() == play)
+		{
+			System.out.println("Holaaaa");
+		}
+		else if(arg0.getSource() == pausa)
+		{
+			System.out.println("Ueeeeee");
+		}
+	}
+
 
 }
