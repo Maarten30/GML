3190f666422385fb16319904425e0deedeb2ebec
gabrigaraizabal <gabrigaraizabal@opendeusto.es>
2018-10-31 10:30:44 +0100

    clase base de datos

---
+++ src/BaseDeDatos/BD.java
@@ -0,0 +1,27 @@
+package BaseDeDatos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class BD 
+{
+	public static void main (String[] args) throws ClassNotFoundException
+	{
+	    Class.forName("org.sqlite.JDBC"); 
+	    
+	    Connection connection = null;
+	    
+	    try
+	    {
+	    	connection = DriverManager.getConnection("jdbc:sqlite:BaseDatosUsuarios.db");
+	    }
+	    catch(SQLException e) 
+	    {
+	    	 System.err.println(e.getMessage());
+	    }
+	}
+
+}
