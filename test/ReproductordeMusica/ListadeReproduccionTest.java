e610a632c8ed882b18c9398329900dc033386479
laullorente17 <laura.llorente@opendeusto.es>
2018-11-15 23:34:18 +0100

    Test Sencillo

--- test/ReproductordeMusica/ListadeReproduccionTest.java
+++ test/ReproductordeMusica/ListadeReproduccionTest.java
@@ -1,5 +1,51 @@
 package ReproductordeMusica;
 
-public class ListadeReproduccionTest {
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+
+public class ListadeReproduccionTest
+{
+	private ListadeReproduccion lista1;
+	private ListadeReproduccion lista2;
+	
+	private final File STONE = new File( "test/res/Demi Lovato - Stone Cold (Official Video).mp3");
+	
+	@Before
+	public void setUp() 
+	{
+		lista1 = new ListadeReproduccion();
+		lista2 = new ListadeReproduccion();
+		lista2.añadir( STONE);
+		
+	}
+	
+	@After
+	public void tearDown() 
+	{
+		lista2.eliminarLista();
+	}
+	
+	@Test
+	public void TestAñadir() 
+	{
+		lista2.añadir( null ); 
+		assertEquals( lista2.PosFichero(1), null );
+
+	}
+
+	@Test
+	public void TestEliminar() 
+	{
+		lista2.eliminarFichero(1);
+		assertEquals( lista2.getSize(), 1);
+	}
+
 
 }
